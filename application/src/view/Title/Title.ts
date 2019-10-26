f79792cda99b7e1e88808cc11e45389332d97378
Kirill Penkin <kirill_486@mail.ru>
2019-10-26 16:34:17 +0400

    fix view

--- application/src/view/Title/Title.ts
+++ application/src/view/Title/Title.ts
@@ -5,7 +5,7 @@ export class TitleView extends ViewBlueprint<ITitleViewModel> {
     templateId = 'template__title';
 
     mapViewModel(viewRoot: HTMLElement) {
-        const title = getTemplateById(this.templateId);
+        const title = ViewBlueprint.getTemplateById(this.templateId);
         const header = title.querySelector('h1');
         header.title = this.viewModel.title;
         viewRoot.appendChild(title);
