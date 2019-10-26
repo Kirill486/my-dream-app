e85b289e19ac6ceaf4176b852a0c4d8e732d629b
Kirill Penkin <kirill_486@mail.ru>
2019-10-26 11:55:44 +0400

    previos ideas about auto mapping

--- application/src/view/ancestor/behavior/argumentProbes.ts
+++ application/src/view/ancestor/behavior/argumentProbes.ts
@@ -4,3 +4,25 @@ export const isValue = (argument: any) =>
     (typeof argument !== 'number') ||
     (typeof argument !== 'boolean');
 export const isCallback = (argument: any) => typeof argument === 'function';
+
+// mapViewModel(viewEntry: HTMLElement) {
+
+    //     const modelKeys = Object.keys(this.viewModel);
+    //     for (let key of modelKeys) {
+            
+    //         const value = this.viewModel[key];
+    //         const correspondingControl = viewEntry.querySelector(`#${key}`);
+
+    //         if (correspondingControl) {
+    //             if (isCallback(value)) {
+    //                 viewEntry.addEventListener('click', value);
+    //             } else if (isValue(value)) {
+    //                 viewEntry.innerHTML = value;                        
+    //             } else {
+    //                 throw new Error(`Unsupported ViewModel value ${value} of type ${typeof value}`);
+    //             }
+    //         } else {
+    //             throw new Error(`Control not found id = ${key}`);
+    //         }            
+    //     }
+    // }
