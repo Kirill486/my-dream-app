20df38a2f212d947d6f435b28931dfa3d38aef59
Kirill Penkin <kirill_486@mail.ru>
2019-10-26 15:34:41 +0400

    upgrade use case

--- application/src/buisiness_logic/useCases/manage/ManageAddToDo.ts
+++ application/src/buisiness_logic/useCases/manage/ManageAddToDo.ts
@@ -1,14 +1,28 @@
 import { IToDo, IApplicationState } from "../../../model/types";
 import { UseCaseBlueprint } from "../ancestor/UseCaseBlueprint";
 
-interface argsDTO {
-    todos: IToDo[];
-}
-
-export class ManageAddToDo extends UseCaseBlueprint<IApplicationState, argsDTO> {
+export class ManageAddToDo extends UseCaseBlueprint<IApplicationState, {}> {
     useCaseTitle = 'ManageAddToDo';
 
-    buisinessLogic(payload) {
-        this.state.toDoList = [...this.state.toDoList, ...payload.todos];
+    static toDoInitialTitle = '....Summarize your note....';
+    static toDoInitialDescription = '....Type here what shoud be done....';
+
+    private createEmptyToDo() {
+        const id = this.state.misc.nextId;
+        const createdTime = Date.now();
+        const title = ManageAddToDo.toDoInitialTitle;
+        const description = ManageAddToDo.toDoInitialDescription;
+        const todo: IToDo = {
+            id,
+            createdTime,
+            title,
+            description,
+            done: false,
+        }
+        return todo;
     }
+
+    buisinessLogic() {
+        this.state.toDoList = [...this.state.toDoList, this.createEmptyToDo()];
+    }    
 }
