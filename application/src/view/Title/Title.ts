9dc618e284ddc2ddbd4aa636c9269a4cf73a2bab
Kirill Penkin <kirill_486@mail.ru>
2019-10-26 13:10:04 +0400

    title and template

--- application/src/view/Title/Title.ts
+++ application/src/view/Title/Title.ts
@@ -1,6 +1,14 @@
 import { ViewBlueprint } from "../ancestor/ViewBlueprint";
 import { ITitleViewModel } from "./types";
+import { getTemplateById } from "../ancestor/behavior/templateHelper";
 
 export class TitleView extends ViewBlueprint<ITitleViewModel> {
     templateId = 'template__title';
+
+    mapViewModel(viewRoot: HTMLElement) {
+        const title = getTemplateById(this.templateId);
+        const header = title.querySelector('h1');
+        header.title = this.viewModel.title;
+        viewRoot.appendChild(title);
+    }
 }
