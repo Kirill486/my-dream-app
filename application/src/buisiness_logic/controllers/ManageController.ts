f98faccd223c7f7df584902fe67d96071d4ddc04
Kirill Penkin <kirill_486@mail.ru>
2019-11-05 16:15:47 +0400

    save use case to the controller

--- application/src/buisiness_logic/controllers/ManageController.ts
+++ application/src/buisiness_logic/controllers/ManageController.ts
@@ -2,9 +2,11 @@ import { ManageAddToDo } from "./useCases/manage/ManageAddToDo";
 import { appStore } from "../../model/Model";
 import { ManageRemoveTodo } from "./useCases/manage/ManageRemoveTodo";
 import { ManageToggleDone } from "./useCases/manage/ManageToggleDone";
+import { ManageSaveToDo } from "./useCases/manage/ManageSaveToDo";
 
 export class ManageController {
     public static add = new ManageAddToDo(appStore);
     public static remove = new ManageRemoveTodo(appStore);
     public static toggleDone = new ManageToggleDone(appStore);
+    public static  save = new ManageSaveToDo(appStore);
 }
