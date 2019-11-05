84eab0cf47b03d6a8e03e428282cce51cf96b1f7
Kirill Penkin <kirill_486@mail.ru>
2019-11-05 12:32:01 +0400

    filter toDos

--- application/src/viewControllers/ToDoListViewController.ts
+++ application/src/viewControllers/ToDoListViewController.ts
@@ -1,19 +1,33 @@
-import { IApplicationState } from '../model/types';
+import { IApplicationState, IToDo, IFilterState } from '../model/types';
 import { ViewControllerBlueprint } from "./ancestor/ViewControllerBlueprint";
 import { ITodoListVM, ITodoListDataVM, ITodoListActionVM } from '../view/ToDoList/ToDoList';
 import { FilterController } from '../buisiness_logic/controllers/FilterController';
 import { ManageController } from '../buisiness_logic/controllers/ManageController';
 
 export class TodoListViewController extends ViewControllerBlueprint<IApplicationState, ITodoListVM> {
+    
+    static filterToDos = (todos: IToDo[], filters: IFilterState) => {
+        const {showDone} = filters;
+        if (showDone) {
+            return todos;
+        } else {
+            return todos.filter((item) => !item.done);
+        }
+    } 
+    
     mapToVm(state: IApplicationState) {
+        
+        const todos = TodoListViewController.filterToDos(state.toDoList, state.filters);
+        
         const todoListDataVM: ITodoListDataVM = {
-            todos: state.toDoList,
+            todos,
             isDoneShown: state.filters.showDone,
         };
         const todoListActionVM: ITodoListActionVM = {
             toggleShowDone: () => FilterController.toggleShowDone.use({}),
             addToDo: () => ManageController.add.use({}),
             switchDoneToDo: (id: any) => ManageController.toggleDone.use({ id }),
+            selectToDo: (id: any) => FilterController.select.use({ id }),
         };
         const toDoListVM: ITodoListVM = {
             ...todoListDataVM,
