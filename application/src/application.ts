77c0f292346294e8d31274cad5ffd121d45245ab
Kirill Penkin <kirill_486@mail.ru>
2019-11-07 15:09:17 +0400

    for debug needs

--- application/src/application.ts
+++ application/src/application.ts
@@ -1,8 +1,20 @@
 import { ViewPool } from "./viewPool/ViewPool";
-import { appStore } from "./model/Model";
+import { appStore, ApplicationModel } from "./model/Model";
 import { BuisinessLogicBlueprint } from "./buisiness_logic/ancestor/BuisinessLogicBlueprint";
 
 const applicationViewPool = new ViewPool(appStore);
 
 BuisinessLogicBlueprint.subscribeToChanges(() => applicationViewPool.render());
 BuisinessLogicBlueprint.fireChangedEvent();
+
+interface IAppDocument extends Document {
+    conserns: {
+        model: ApplicationModel,
+        buisinessLogic: typeof BuisinessLogicBlueprint,
+    }
+}
+
+(document as IAppDocument).conserns = {
+    model: appStore,
+    buisinessLogic: BuisinessLogicBlueprint,
+};
