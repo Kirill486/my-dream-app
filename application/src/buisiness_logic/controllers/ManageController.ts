e0cd851d6cf5f04ada8a11516790e78caa212dec
Kirill Penkin <kirill_486@mail.ru>
2019-11-08 15:17:06 +0400

    new use case

--- application/src/buisiness_logic/controllers/ManageController.ts
+++ application/src/buisiness_logic/controllers/ManageController.ts
@@ -5,10 +5,12 @@ import { ManageToggleDone } from "./useCases/manage/ManageToggleDone";
 import { ManageSaveToDo } from "./useCases/manage/ManageSaveToDo";
 import { applicationStorage } from "../../buisiness_repository/PermanentStorage";
 import { logger } from "../../buisiness_logger/Logger";
+import { ManageRestoreToDos } from "./useCases/manage/ManageRestoreToDos";
 
 export class ManageController {
     public static add = new ManageAddToDo(appStore, applicationStorage, logger);
     public static remove = new ManageRemoveTodo(appStore, applicationStorage, logger);
     public static toggleDone = new ManageToggleDone(appStore, applicationStorage, logger);
     public static  save = new ManageSaveToDo(appStore, applicationStorage, logger);
+    public static restore = new ManageRestoreToDos(appStore, applicationStorage, logger);
 }
