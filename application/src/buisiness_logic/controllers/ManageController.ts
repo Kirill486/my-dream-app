6ce74653174ba14689e35c3483be2b5fab083ff2
Kirill Penkin <kirill_486@mail.ru>
2019-11-08 14:44:55 +0400

    pass new services to controllers

--- application/src/buisiness_logic/controllers/ManageController.ts
+++ application/src/buisiness_logic/controllers/ManageController.ts
@@ -3,10 +3,12 @@ import { appStore } from "../../model/Model";
 import { ManageRemoveTodo } from "./useCases/manage/ManageRemoveTodo";
 import { ManageToggleDone } from "./useCases/manage/ManageToggleDone";
 import { ManageSaveToDo } from "./useCases/manage/ManageSaveToDo";
+import { applicationStorage } from "../../buisiness_repository/PermanentStorage";
+import { logger } from "../../buisiness_logger/Logger";
 
 export class ManageController {
-    public static add = new ManageAddToDo(appStore);
-    public static remove = new ManageRemoveTodo(appStore);
-    public static toggleDone = new ManageToggleDone(appStore);
-    public static  save = new ManageSaveToDo(appStore);
+    public static add = new ManageAddToDo(appStore, applicationStorage, logger);
+    public static remove = new ManageRemoveTodo(appStore, applicationStorage, logger);
+    public static toggleDone = new ManageToggleDone(appStore, applicationStorage, logger);
+    public static  save = new ManageSaveToDo(appStore, applicationStorage, logger);
 }
