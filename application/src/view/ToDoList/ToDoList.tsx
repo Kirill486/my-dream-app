4c835db168fbe9471497c36437d65dbce8058bb7
Kirill Penkin <kirill_486@mail.ru>
2019-11-16 12:42:59 +0400

    list

--- application/src/view/ToDoList/ToDoList.tsx
+++ application/src/view/ToDoList/ToDoList.tsx
@@ -1,17 +1,44 @@
 import * as React from 'react';
 import {ITodoListVM} from './types';
+import { ToDoListItem } from './subcomponent/ToDoListItem';
 import {doNothing} from '../../domain_types/definitions'
+import { IToDoListItemProps } from './subcomponent/types';
 
-export const ToDoList: React.FunctionComponent<ITodoListVM> = () => {
+export const ToDoList: React.FunctionComponent<ITodoListVM> =
+(props: ITodoListVM) => {
     return (
         <div className="todo-list__container">
             <div className="todo-list__title-row">
-                <button className="todo-list__add-todo background-color-animated">+</button>
+                <button 
+                    className="todo-list__add-todo background-color-animated"
+                    onClick={props.addToDo}
+                >
+                        +
+                </button>
                 <div className="todo-list__show-done-container">
-                    <input className="todo-list__show-done" type="checkbox">Show done</input>
+                    <label htmlFor="show-done">Show Done</label>
+                    <input 
+                        id="show-done"
+                        className="todo-list__show-done" 
+                        type="checkbox" 
+                        checked={props.isDoneShown}
+                        onChange={() => {
+                            props.toggleShowDone();
+                        }}
+                    />                                       
                 </div>
             </div>
-            <div className="todo-list__items-container"></div>
+            <div className="todo-list__items-container">
+                {props.todos.map((item) => {
+                    const itemProps: IToDoListItemProps = {
+                        todo: item,
+                        selectToDO: () => props.selectToDo(item.id),
+                        switchDone: () => props.switchDoneToDo(item.id),
+                        isSelected: item.id === props.selectedToDoId,
+                    }
+                    return (<ToDoListItem {...itemProps} key={item.id} />)
+                })}
+            </div>
         </div>
     );
 }
