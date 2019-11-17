8c0071b912dc99b0d2706bd50072b3dae5700bb4
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 12:51:52 +0400

    better developer experience with devtools

--- application/src/model/configureStore.ts
+++ application/src/model/configureStore.ts
@@ -3,6 +3,7 @@ import { filtersReducer } from "./reducers/filtersReducer";
 import { toDoListReducer } from "./reducers/toDoListReducer";
 import { miscReducer } from "./reducers/miscReducer";
 import { IApplicationState } from "../domain_types/types";
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const applicationReducer = combineReducers({
     toDoList: toDoListReducer,
@@ -10,4 +11,4 @@ const applicationReducer = combineReducers({
     misc: miscReducer,
 });
 
-export const applicationModel: Store<IApplicationState> = createStore(applicationReducer);
+export const applicationModel: Store<IApplicationState> = createStore(applicationReducer, composeWithDevTools());
