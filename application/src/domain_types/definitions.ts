31917f08ef34dc8e5b20fa05cdb9f76fd44fd5e4
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 11:36:14 +0400

    app action definition

--- application/src/domain_types/definitions.ts
+++ application/src/domain_types/definitions.ts
@@ -1,3 +1,11 @@
+import { Action } from "redux";
+
 export const doNothing = () => {
     // do nothing
 }
+
+export interface IPayloadAction<Payload> extends Action {
+    payload: Payload;
+}
+
+export type app_id = number;
