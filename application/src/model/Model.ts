3cbbc0bd08489a9beb52df71310f9d69e5ea8170
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 14:24:31 +0400

    Revert "no longer need for old model"
    This reverts commit 31a060f8ff7e4cd44db350f0cd3aed2c4e44e074.

---
+++ application/src/model/Model.ts
@@ -0,0 +1,59 @@
+import { ModelBlueprint } from "./ancestor/ModelBlueprint";
+import { IApplicationState } from "../domain_types/types";
+import { LoggingService } from "../logging_service/LoggingService";
+
+const initialState: IApplicationState = {
+    filters: {
+        selected: null,
+        showDone: true,
+    },
+    toDoList: [],
+    misc: {
+        isModelSyncronized: true, // not implemented yet
+        logQuenue: [], // not implemented yet
+    }
+}
+
+let nextId = 1
+
+export class ApplicationModel extends ModelBlueprint<IApplicationState> {
+    // private state: S;
+
+    constructor(initialState) {
+        super();
+        this.state = initialState;
+    }
+
+    // I feel like we need a ModelControllerHere.
+    public findLastId(): void {
+        let lastId = nextId;
+        this.state.toDoList.forEach((item) => {
+            if (item.id > lastId) {
+                lastId = item.id;
+            }
+        });
+        nextId = lastId + 1;
+    }
+
+    public getNextId(): number {
+        return nextId++;
+    }
+
+    public getState() {
+        return JSON.parse(JSON.stringify(this.state));
+    }
+
+    public setState(state) {
+        this.state = state;
+        LoggingService.logToConsoleTable(state);
+    }
+
+    public shouldManuallySync = true;
+
+    public dispatch() {
+        // old model does not provide any action-based API
+        throw new Error('Dispatch is not implemented');
+    }
+}
+
+// export const appStore = new ApplicationModel(initialState);
