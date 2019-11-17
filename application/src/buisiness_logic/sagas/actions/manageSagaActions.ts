b35338e7e07bc046b4328852944041ab1f1993d6
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 18:11:11 +0400

    saga actions

---
+++ application/src/buisiness_logic/sagas/actions/manageSagaActions.ts
@@ -0,0 +1,32 @@
+import { ActionCreator, Action } from "redux"
+import { IPayloadAction, app_id } from "../../../domain_types/definitions"
+import { manageActionTypes } from "./sagaActionTypes"
+import { IToDo } from "../../../domain_types/types"
+
+export const addAction: ActionCreator<Action> =
+() => ({
+    type: manageActionTypes.add,
+})
+
+export const removeAction: ActionCreator<IPayloadAction<any>> =
+(id: app_id) => ({
+    type: manageActionTypes.remove,
+    payload: id,
+})
+
+export const toggleDoneAction: ActionCreator<IPayloadAction<app_id>> =
+(id: app_id) => ({
+    type: manageActionTypes.toggleDone,
+    payload: id,
+})
+
+export const saveAction: ActionCreator<IPayloadAction<IToDo>> =
+(todo: IToDo) => ({
+    type: manageActionTypes.save,
+    payload: todo,
+})
+
+export const restoreAction: ActionCreator<Action> =
+() => ({
+    type: manageActionTypes.restore,
+})
