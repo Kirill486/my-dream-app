a741a349ceee178457fab28affbd3e7759508e1e
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 18:48:52 +0400

    saga for each phase

--- application/src/buisiness_logic/sagas/sagas/shared/shared.ts
+++ application/src/buisiness_logic/sagas/sagas/shared/shared.ts
@@ -3,14 +3,19 @@ import { Action } from "redux";
 import { logger } from "../../../../buisiness_logger/Logger";
 import { call, select } from "@redux-saga/core/effects";
 import { applicationStorage } from "../../../../buisiness_repository/PermanentStorage";
+import { BuisinessLogicBlueprint } from "../../../ancestor/BuisinessLogicBlueprint";
 
 export function* logUseCase (action: Action) {
     yield call(logger.LogOne.bind(logger), getInfo(action));
 }
 
 export function* syncModelWithStorage() {
-    const appState = select();
+    const appState = yield select();
     yield call(applicationStorage.store.bind(applicationStorage), appState);
+}
+
+export function* fireChangedEvent() {
+    yield call(BuisinessLogicBlueprint.fireChangedEvent); 
 }
 
 const getInfo = (action: Action) => {
