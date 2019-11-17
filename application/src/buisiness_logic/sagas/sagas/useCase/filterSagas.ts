5b8be1b05cdd0bdb39c60f8f46a4b621157dada5
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 17:43:30 +0400

    filter copy pastle buisiness logic

--- application/src/buisiness_logic/sagas/sagas/useCase/filterSagas.ts
+++ application/src/buisiness_logic/sagas/sagas/useCase/filterSagas.ts
@@ -1,13 +1,17 @@
 import { Action } from "redux";
-import { call } from "@redux-saga/core/effects";
+import { call, put } from "@redux-saga/core/effects";
 import { logger } from "../../../../buisiness_logger/Logger";
+import { IPayloadAction, app_id } from "../../../../domain_types/definitions";
+import { setSelected } from "../../../../model/actions/filterActions";
+import {toggleShowDone as toggleShowDoneModelAction} from '../../../../model/actions/filterActions'
 
 export function* select (action: Action) {
     yield call(logger.LogOne.bind(logger), `select`);
-    // yield put(statusMessage(statusMessages.LOADING));
+    const {payload: id} = action as IPayloadAction<app_id>;
+    yield put(setSelected(id));
 }
 
 export function* toggleShowDone (action: Action) {
     yield call(logger.LogOne.bind(logger), `toggleShowDone`);
-    // yield put(statusMessage(statusMessages.LOADING));
+    yield put(toggleShowDoneModelAction());
 }
