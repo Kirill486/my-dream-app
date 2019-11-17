19a96698a9d65b291df1e410b1a358f5342a9e43
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 15:48:11 +0400

    saga action types one for each controller method

---
+++ application/src/buisiness_logic/sagas/actions/sagaActionTypes.ts
@@ -0,0 +1,23 @@
+//here we just list our use cases again
+
+const sagaActionPrefix = 'SAGA_';
+const manageActionPrefix = 'MANAGE_'
+const filterActionPrefix = 'FILTER_';
+
+export const filterSagaActionTypes = Object.freeze({
+    select: `${sagaActionPrefix}${filterActionPrefix}SELECT`,
+    toggleShowDone: `${sagaActionPrefix}${filterActionPrefix}TOGGLE_SHOW_DONE`,
+});
+
+export const manageActionTypes = Object.freeze({
+    add: `${sagaActionPrefix}${manageActionPrefix}ADD`,
+    remove: `${sagaActionPrefix}${manageActionPrefix}REMOVE`,
+    toggleDone: `${sagaActionPrefix}${manageActionPrefix}TOGGLE_DONE`,
+    save: `${sagaActionPrefix}${manageActionPrefix}SAVE`,
+    restore: `${sagaActionPrefix}${manageActionPrefix}RESTORE`,
+})
+
+export const sagaActionTypes = Object.freeze({
+    ...filterSagaActionTypes,
+
+});
