dfc33e0806c8928b7cb503d6cc20a36310dd6486
Kirill Penkin <kirill_486@mail.ru>
2019-11-17 16:05:52 +0400

    and root saga to rule them all

--- application/src/buisiness_logic/sagas/rootSaga.ts
+++ application/src/buisiness_logic/sagas/rootSaga.ts
@@ -1,15 +1,18 @@
 import { logger } from "../../buisiness_logger/Logger"
-import { call } from "@redux-saga/core/effects";
+import { call, takeEvery, select } from "@redux-saga/core/effects";
+import { sagaActionTypes, manageActionTypes, filterSagaActionTypes } from "./actions/sagaActionTypes";
+import { add, remove, restore, save, toggleDone } from "./sagas/manageSagas";
 
 export function* rootSaga() {
-    // root saga syntax
-    // yield takeEvery(action.type, correspondingSaga);
-    yield call(logger.LogOne.bind(logger), `rootSaga`);
+    
+    // manage controller
+    yield takeEvery(manageActionTypes.add, add);
+    yield takeEvery(manageActionTypes.remove, remove);
+    yield takeEvery(manageActionTypes.restore, restore);
+    yield takeEvery(manageActionTypes.save, save);
+    yield takeEvery(manageActionTypes.toggleDone, toggleDone);
+
+    // filter controller
+    yield takeEvery(filterSagaActionTypes.select, select);
+    yield takeEvery(filterSagaActionTypes.toggleShowDone, select);    
 }
-
-// correspondingSagasSyntax
-
-// export function* longSaga (action: IAction) {
-//     yield call(console.warn, `hello long saga ${action.type}`);
-//     yield put(statusMessage(statusMessages.LOADING));
-// }
